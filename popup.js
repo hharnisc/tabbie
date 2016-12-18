5258ef8857b15e873e066aefdbc677df9f35f7ba
Harrison Harnisch <hharnisc@gmail.com>
2016-12-18 10:29:38 -0600

    A list of selected url tabs

--- popup.js
+++ popup.js
@@ -1,3 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-  console.log('hi');
+const getSelectedTabs = () => new Promise((resolve) => {
+  chrome.tabs.query({
+      highlighted: true,
+      lastFocusedWindow: true,
+  }, (tabs) => {
+      resolve(tabs.map((tab) => tab.url));
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  getSelectedTabs()
+    .then((tabs) => console.log(tabs));
 });
