f7aefe76e57b6f21c56337099354959ef3646d7f
Harrison Harnisch <hharnisc@gmail.com>
2016-12-29 11:34:13 -0600

    Add event for app open

--- analytics.js
+++ analytics.js
@@ -7,3 +7,4 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-89553254-1', 'auto');
 ga('set', 'checkProtocolTask', function(){}); // Removes failing protocol check. @see: http://stackoverflow.com/a/22152353/1958200
 ga('require', 'displayfeatures');
+ga('send', 'pageview', '/');
