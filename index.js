29e165ede531a99429275863b08ee7bbea321cbc
Harrison Harnisch <hharnisc@gmail.com>
2017-01-07 09:54:52 -0600

    hello react!

---
+++ index.js
@@ -0,0 +1,8 @@
+let React = require('react');
+let ReactDOM = require('react-dom');
+let App = require('./src/app');
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('container')
+);
---
+++ src/app.js
@@ -0,0 +1,3 @@
+let React = require('react');
+
+module.exports = () => <div>Hi!</div>;
