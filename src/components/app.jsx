5244cd30596c5073ed1ffff3f6e615364bee87ce
Harrison Harnisch <hharnisc@gmail.com>
2017-01-08 13:12:44 -0600

    Stub in list component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import List from '../components/List';
 import ListControls from '../containers/ListControls';
 
 export default () =>
   <div>
     <h2>Saved Tab Groups</h2>
+    <List />
     <ListControls />
   </div>;
