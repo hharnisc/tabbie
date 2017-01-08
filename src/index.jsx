629335e14396c860171142f27d73bf76cdcee6fe
Harrison Harnisch <hharnisc@gmail.com>
2017-01-08 12:31:24 -0600

    set initial redux state with chrome sync storage

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { getState } from './chromeStorage';
 import App from './components/app';
 import tabbieApp from './reducers';
 
-const store = createStore(tabbieApp);
-
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('container'),
-);
+getState()
+  .then((state) => {
+    const store = createStore(tabbieApp, {
+      controls: {
+        saveSelected: state.saveSelected,
+      },
+    });
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      document.getElementById('container'),
+    );
+  });
