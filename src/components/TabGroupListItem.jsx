97334c9474dcd1ea01ff0365249f78d72ed84f45
Harrison Harnisch <hharnisc@gmail.com>
2017-01-23 19:00:35 -0600

    Add open and remove buttons

--- src/components/TabGroupListItem.jsx
+++ src/components/TabGroupListItem.jsx
@@ -1,7 +1,11 @@
 import React, { PropTypes } from 'react';
 
 const TabGroupListItem = ({ name }) =>
-  <div>{ name }</div>;
+  <div>
+    { name }
+    <button>Open</button>
+    <button>Remove</button>
+  </div>;
 
 TabGroupListItem.propTypes = {
   name: PropTypes.string,
