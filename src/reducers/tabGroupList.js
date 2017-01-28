d047b0cb04f8b8ed059bff48e5268507944c207e
Harrison Harnisch <hharnisc@gmail.com>
2017-01-28 14:27:03 -0600

    cleanup console log

--- src/reducers/tabGroupList.js
+++ src/reducers/tabGroupList.js
@@ -3,7 +3,6 @@ import { REMOVE_TAB_GROUP } from '../actions';
 const tabGroupList = (state = [], action) => {
   switch (action.type) {
     case REMOVE_TAB_GROUP:
-      console.log('filtered tabGroups', state.tabGroups.filter((tabGroup, i) => i !== action.tabGroupKey));
       return {
         tabGroups: state.tabGroups.filter((tabGroup, i) => i !== action.tabGroupKey),
       };
