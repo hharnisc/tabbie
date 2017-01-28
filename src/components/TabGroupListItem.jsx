442d4f69c0b9668ee23f1e75a7e94be9b332f50a
Harrison Harnisch <hharnisc@gmail.com>
2017-01-28 13:29:47 -0600

    add open click handler

--- src/components/TabGroupListItem.jsx
+++ src/components/TabGroupListItem.jsx
@@ -1,15 +1,16 @@
 import React, { PropTypes } from 'react';
 
-const TabGroupListItem = ({ name, onRemoveClick }) =>
+const TabGroupListItem = ({ name, onRemoveClick, onOpenClick }) =>
   <div>
     { name }
-    <button>Open</button>
+    <button onClick={onOpenClick}>Open</button>
     <button onClick={onRemoveClick}>Remove</button>
   </div>;
 
 TabGroupListItem.propTypes = {
   name: PropTypes.string,
   onRemoveClick: PropTypes.func,
+  onOpenClick: PropTypes.func,
 };
 
 export default TabGroupListItem;
--- src/containers/TabGroupListItem.js
+++ src/containers/TabGroupListItem.js
@@ -4,6 +4,7 @@ import TabGroupListItem from '../components/TabGroupListItem';
 const mapStateToProps = (state, ownProps) => ownProps;
 const mapDispatchToProps = dispatch => ({
   onRemoveClick: () => console.log('remove'),
+  onOpenClick: () => console.log('open'),
 });
 
 export default connect(
