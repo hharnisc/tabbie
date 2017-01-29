a902ef8f2306d7571296bd2cbd3b24d89652df1d
Harrison Harnisch <hharnisc@gmail.com>
2017-01-29 11:08:35 -0600

    set app style

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import TabGroupList from '../containers/TabGroupList';
 import TabGroupListControls from '../containers/TabGroupListControls';
 
+const style = {
+  height: '40em',
+  width: '30em',
+  padding: '1em',
+};
+
 export default () =>
-  <div>
+  <div style={style}>
     <h2>Saved Tab Groups</h2>
     <TabGroupList />
     <TabGroupListControls />
