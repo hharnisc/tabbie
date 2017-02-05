e63ef321c4642b8b3627b051a4b47e06f68f5227
Harrison Harnisch <hharnisc@gmail.com>
2017-02-05 10:53:22 -0600

    style list item buttons

--- src/components/TabGroupListItem.jsx
+++ src/components/TabGroupListItem.jsx
@@ -1,19 +1,34 @@
 import React, { PropTypes } from 'react';
+import {
+  buttonStyle,
+  warningButtonStyle,
+} from '../styles/button';
 
 const listItemStyle = {
   display: 'flex',
   padding: '0.5em 0',
 };
 
-const listItemName = {
+const listItemNameStyle = {
   flexGrow: 1,
 };
 
+
 const TabGroupListItem = ({ tabGroupKey, name, onRemoveClick, onOpenClick, tabs }) =>
   <li style={listItemStyle}>
-    <span style={listItemName}>{ name }</span>
-    <button onClick={() => onOpenClick(tabs)}>Open</button>
-    <button onClick={() => onRemoveClick(tabGroupKey)}>Remove</button>
+    <span style={listItemNameStyle}>{ name }</span>
+    <button
+      onClick={() => onOpenClick(tabs)}
+      style={{ ...buttonStyle, marginLeft: 10 }}
+    >
+      open
+    </button>
+    <button
+      onClick={() => onRemoveClick(tabGroupKey)}
+      style={{ ...warningButtonStyle, marginLeft: 10 }}
+    >
+      remove
+    </button>
   </li>;
 
 TabGroupListItem.propTypes = {
---
+++ src/styles/button.js
@@ -0,0 +1,12 @@
+export const buttonStyle = {
+  background: '#fff',
+  borderRadius: '3px',
+  border: 'none',
+  color: '#4285f4',
+  padding: '0.5em',
+};
+
+export const warningButtonStyle = {
+  ...buttonStyle,
+  color: '#ea4335',
+};
