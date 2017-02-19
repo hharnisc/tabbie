d32199288fe950c989fcd91c5c6bbe5650e0d18f
Harrison Harnisch <hharnisc@gmail.com>
2017-02-19 08:54:28 -0600

    remove unneeded app file

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import App from './components/app';
+import TabGroupListView from './components/TabGroupListView';
+import TabGroupDetailsView from './containers/TabGroupDetailsView';
 import tabbieApp from './reducers';
 import analytics from './middleware/analytics';
 import chromeStorage from './middleware/chromeStorage';
 import tabManager from './middleware/tabManager';
+
+const appStyle = {
+  padding: '2em',
+  height: '40em',
+  width: '30em',
+  boxSizing: 'border-box',
+};
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = createStore(tabbieApp, applyMiddleware(
@@ -18,7 +27,12 @@ document.addEventListener('DOMContentLoaded', () => {
   ));
   render(
     <Provider store={store}>
-      <App />
+      <div style={appStyle}>
+        <Router history={browserHistory}>
+          <Route path="/details/:tabGroupKey" component={TabGroupDetailsView} />
+          <Route path="*" component={TabGroupListView} />
+        </Router>
+      </div>
     </Provider>,
     document.getElementById('container'),
   );
